2492d5f11faaf9b3367c7d7be2a6a278409acc0e
LJH16 <lvjinhong16@gmail.com>
2019-06-07 17:05:23 +0800

    Site updated: 2019-06-07 17:05:22

--- dist/music.js
+++ dist/music.js
@@ -1,24 +1,10 @@
 const ap = new APlayer({
     container: document.getElementById('player'),
-    listFolded: false,//列表默认折叠
-    listMaxHeight: 90,//列表最大高度
-   
-    audio: [
-        {
-            name: '夜空中最亮的星',
-            artist: 'artist1',
-            url: 'http://www.ytmp3.cn/down/34031.mp3',
-            cover: 'cover1.jpg',
-            lrc: 'lrc1.lrc',
-            theme: '#ebd0c2'
-        },
-        {
-            name: 'name2',
-            artist: 'artist2',
-            url: 'http://www.ytmp3.cn/down/59015.mp3',
-            cover: 'cover2.jpg',
-            lrc: 'lrc2.lrc',
-            theme: '#46718b'
-        }
-    ]
+    fixed: true,
+    audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: 'https://m10.music.126.net/20190607172533/803ad005327240f2e1ec1bdf489c2c55/ymusic/525d/540b/510f/e7403c0f89ca574eea7dfea2ac7601f5.mp3',
+        cover: 'cover.jpg',
+    }]
 });
