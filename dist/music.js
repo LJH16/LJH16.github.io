a46d1b367f402843164690ea4a1f4815d5703135
LJH16 <lvjinhong16@gmail.com>
2019-06-07 16:34:13 +0800

    Site updated: 2019-06-07 16:34:12

--- dist/music.js
+++ dist/music.js
@@ -1,10 +1,19 @@
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
+     container: document.getElementById('aplayer'),
+     mutex: true, //互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器
+      preload: 'auto', //预加载，可选值: 'none', 'metadata', 'auto'
     audio: [{
-      mutex: true, //���⣬��ֹ���������ͬʱ���ţ���ǰ����������ʱ��ͣ����������
-        name: 'ҹ������������',
-        artist: '���ܼƻ�',
+        name: '夜空中最亮的星',
+        artist: '逃跑计划',
         url: 'http://www.ytmp3.cn/down/34031.mp3',
         cover: 'cover.jpg'
-    }]
+    }，
+     {
+            name: 'name2', //如果只有一首歌，删掉这一块，如有更多歌曲按此格式逐渐往下添加
+            artist: 'artist2',
+            url: 'url2.mp3',
+            cover: 'cover2.jpg',
+            lrc: 'lrc2.lrc',
+            theme: '#46718b'
+        }
 });
