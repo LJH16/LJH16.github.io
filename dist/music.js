a1385e87f4d1f9346f9f86d82b6b35dc64c39943
LJH16 <lvjinhong16@gmail.com>
2019-06-07 17:40:59 +0800

    Site updated: 2019-06-07 17:40:57

--- dist/music.js
+++ dist/music.js
@@ -9,7 +9,7 @@ const ap = new APlayer({
         {
             name: '����',
             artist: 'artist1',
-            url: ' https://m10.music.126.net/20190607172533/803ad005327240f2e1ec1bdf489c2c55/ymusic/525d/540b/510f/e7403c0f89ca574eea7dfea2ac7601f5.mp3',
+            url: 'https://m10.music.126.net/20190607172533/803ad005327240f2e1ec1bdf489c2c55/ymusic/525d/540b/510f/e7403c0f89ca574eea7dfea2ac7601f5.mp3',
             cover: 'cover1.jpg',
         },
         {
