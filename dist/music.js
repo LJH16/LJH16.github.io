1b935f6a54be4503a6c6ae450db72f88755e38db
LJH16 <lvjinhong16@gmail.com>
2019-06-07 16:23:02 +0800

    Site updated: 2019-06-07 16:23:01

--- dist/music.js
+++ dist/music.js
@@ -3,7 +3,7 @@ const ap = new APlayer({
     audio: [{
         name: 'name',
         artist: 'artist',
-        url: '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=25706282&auto=1&height=66"></iframe>',
+        url: 'http://www.ytmp3.cn/down/51257.mp3',
         cover: 'cover.jpg'
     }]
 });
