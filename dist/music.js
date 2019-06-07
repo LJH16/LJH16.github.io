7cc202c58339fb8621c568f7e9892efb9240db29
LJH16 <lvjinhong16@gmail.com>
2019-06-07 16:19:16 +0800

    Site updated: 2019-06-07 16:19:14

--- dist/music.js
+++ dist/music.js
@@ -3,7 +3,7 @@ const ap = new APlayer({
     audio: [{
         name: 'name',
         artist: 'artist',
-        url: 'url.mp3',
+        url: '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=25706282&auto=1&height=66"></iframe>',
         cover: 'cover.jpg'
     }]
 });
