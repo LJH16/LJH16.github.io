120e4b3afd72394da2bc1690756bb4953e4f75ea
LJH16 <lvjinhong16@gmail.com>
2019-06-07 17:35:58 +0800

    Site updated: 2019-06-07 17:35:57

--- dist/music.js
+++ dist/music.js
@@ -7,15 +7,15 @@ const ap = new APlayer({
     listMaxHeight: 60,
     audio: [
         {
-            name: 'name1',
+            name: '����',
             artist: 'artist1',
             url: ' https://m10.music.126.net/20190607172533/803ad005327240f2e1ec1bdf489c2c55/ymusic/525d/540b/510f/e7403c0f89ca574eea7dfea2ac7601f5.mp3',
             cover: 'cover1.jpg',
         },
         {
-            name: 'name2',
-            artist: 'http://www.ytmp3.cn/down/34031.mp3',
-            url: 'url2.mp3',
+            name: 'ҹ������������',
+            artist: '���ܼƻ�',
+            url: 'http://www.ytmp3.cn/down/34031.mp3',
             cover: 'cover2.jpg',
         }
     ]
