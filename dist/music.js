fe3ec790e6ad95e7560c2d7eddccff7c113b04dc
LJH16 <lvjinhong16@gmail.com>
2019-06-07 17:43:04 +0800

    Site updated: 2019-06-07 17:43:02

--- dist/music.js
+++ dist/music.js
@@ -7,14 +7,14 @@ const ap = new APlayer({
     listMaxHeight: 60,
     audio: [
         {
-            name: '����',
+            name: 'wocheng',
             artist: 'artist1',
             url: 'https://m10.music.126.net/20190607172533/803ad005327240f2e1ec1bdf489c2c55/ymusic/525d/540b/510f/e7403c0f89ca574eea7dfea2ac7601f5.mp3',
             cover: 'cover1.jpg',
         },
         {
-            name: 'ҹ������������',
-            artist: '���ܼƻ�',
+            name: 'yekongzhongzhuiliangdexin',
+            artist: 'artist2',
             url: 'http://www.ytmp3.cn/down/34031.mp3',
             cover: 'cover2.jpg',
         }
