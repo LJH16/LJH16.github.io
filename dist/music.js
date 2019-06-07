febf31de2079662a89740a0ad442497cf4b06474
LJH16 <lvjinhong16@gmail.com>
2019-06-07 16:50:03 +0800

    Site updated: 2019-06-07 16:50:00

--- dist/music.js
+++ dist/music.js
@@ -2,7 +2,7 @@ const ap = new APlayer({
     container: document.getElementById('player'),
     listFolded: false,//列表默认折叠
     listMaxHeight: 90,//列表最大高度
-    lrcType: 3, //此为歌词格式，没有歌词可以直接删掉这一行
+   
     audio: [
         {
             name: '夜空中最亮的星',
